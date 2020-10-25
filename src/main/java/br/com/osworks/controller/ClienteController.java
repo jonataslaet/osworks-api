ba7a6bba3b203072e3931810348336bbe3e9b501
jonataslaet <jonatas@infoway-pi.com.br>
2020-10-25 12:19:40 -0300

    Adicionado o flyway ao projeto iniciado com o modelo de entitymanager persistence context posteriormente vai ser alterado

--- src/main/java/br/com/osworks/controller/ClienteController.java
+++ src/main/java/br/com/osworks/controller/ClienteController.java
@@ -3,6 +3,9 @@ package br.com.osworks.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,20 +14,12 @@ import br.com.osworks.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	EntityManager em;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> buscarCliente() {
-		Cliente cli1 = new Cliente();
-		cli1.setId(1L);
-		cli1.setNome("Jonatas Laet");
-		cli1.setTelefone("86 9 88894625");
-		cli1.setEmail("jonataslaet@gmail.com");
 		
-		Cliente cli2 = new Cliente();
-		cli2.setId(2L);
-		cli2.setNome("Jonatas Blendo");
-		cli2.setTelefone("86 9 94643741");
-		cli2.setEmail("jonatas@gmail.com");
-		
-		return Arrays.asList(cli1, cli2);
+		return em.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
