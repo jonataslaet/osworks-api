a7e8be69f189723d93987c27bd1957201b26690f
jonataslaet <jonatas@infoway-pi.com.br>
2020-10-27 00:24:59 -0300

    Ajustes em Cliente colocado o groups no id removido algumas linhas inúteis do service

--- src/main/java/br/com/osworks/service/ClienteService.java
+++ src/main/java/br/com/osworks/service/ClienteService.java
@@ -2,13 +2,9 @@ package br.com.osworks.service;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.osworks.model.Cliente;
 import br.com.osworks.repository.ClienteRepository;
